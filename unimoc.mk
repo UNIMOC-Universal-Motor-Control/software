4a1b51de4985c74c55714a90b00e49f4763363ff
Alex B <tecnologic86@gmail.com>
2019-11-23 15:05:16 +0100

    reorgenised for later multi hardware support

---
+++ unimoc.mk
@@ -0,0 +1,12 @@
+# Base directory of Project
+BASEDIR = ../../
+
+# List of all the board related files.
+UNIMOCSRC = ${BASEDIR}/src/main.cpp \
+
+# Required include directories
+UNIMOCINC = ${BASEDIR}/inc \
+
+# Shared variables
+ALLCSRC += $(UNIMOCSRC)
+ALLINC  += $(UNIMOCINC)
