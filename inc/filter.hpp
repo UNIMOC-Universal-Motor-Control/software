5ca7ab7923f5237395bcc95d2a6aaf4afc4b8ef4
Alexander Brand <tecnologic86@gmail.com>
2020-02-20 14:17:07 +0100

    added biquad filters

--- inc/filter.hpp
+++ inc/filter.hpp
@@ -51,6 +51,120 @@ public:
 	float Calculate(const float uk, const float yk_1);
 };
 
+
+/**
+ * Created by Nigel Redmon on 11/24/12
+ * EarLevel Engineering: earlevel.com
+ * Copyright 2012 Nigel Redmon
+ *
+ * For a complete explanation of the Biquad code:
+ * http://www.earlevel.com/main/2012/11/26/biquad-c-source-code/
+ *
+ * License:
+ *
+ * This source code is provided as is, without warranty.
+ * You may copy and distribute verbatim copies of this document.
+ * You may modify and use this source code to create binary code
+ * for your own purposes, free or commercial.
+ *
+ */
+
+///< Filter types for the biquad class
+enum class biquad_type_ec
+{
+    lowpass = 0,//!< lowpass
+    highpass,   //!< highpass
+    bandpass,   //!< bandpass
+    notch,      //!< notch
+    peak,       //!< peak
+    lowshelf,   //!< lowshelf
+    highshelf   //!< highshelf
+};
+
+/**
+ * Class for second order IIR filter in direct transposed form II
+ */
+class biquad
+{
+public:
+	/**
+	 * Constructor of biquad second order IIR filter
+	 * @param type	type of filter
+	 * @param Fc	corner frequency normed to 1/(sampling frequency)
+	 * @param Q		quality factor of the filter response
+	 * @param peak_gain gain of the filter.
+	 */
+    biquad(const biquad_type_ec type, const float Fc, const float Q, const float peak_gain);
+
+    /**
+     * set type of biquad filter
+     * @param type new type of filter
+     */
+    void SetType(const biquad_type_ec type);
+
+    /**
+     * set the quality factor of the filter
+     * @param Q new quality factor
+     */
+    void SetQ(const float Q);
+
+    /**
+     * set new corner frequency
+     * @param Fc new corner frequency
+     */
+    void SetFc(const float Fc);
+
+    /**
+     * set new gain of the filter
+     * @param peak_gain in db
+     */
+    void SetPeakGain(const float peak_gain);
+
+    /**
+     * set all relevant biquad parameters at once
+     * @param type new type
+     * @param Fc new corner freq.
+     * @param Q new quality factor
+     * @param peak_gain new gain
+     */
+    void SetBiquad(const biquad_type_ec type, const float Fc, const float Q, const float peak_gain);
+
+    /**
+     * calculate the filter
+     * @param in new input to the filter
+     * @return output of the filter
+     */
+    float Process(const float in);
+
+protected:
+    /**
+     * calculate internal coefficients
+     */
+    void CalcBiquad(void);
+
+    ///< type of filter
+    biquad_type_ec type;
+    ///< internal coefficients
+    float a0, a1, a2, b1, b2;
+    ///< settings of the filter
+    float Fc, Q, peak_gain;
+    ///< internal unit delay states
+    float z1, z2;
+};
+
+/**
+ * calculate the filter
+ * @param in new input to the filter
+ * @return output of the filter
+ */
+inline float biquad::Process(const float in)
+{
+    float out = in * a0 + z1;
+    z1 = in * a1 + z2 - b1 * out;
+    z2 = in * a2 - b2 * out;
+    return out;
+}
+
 } /* namespace filter */
 
 #endif /* INC_FILTER_HPP_ */
--- src/filter.cpp
+++ src/filter.cpp
@@ -18,6 +18,7 @@
  */
 #include <filter.hpp>
 #include <cstdint>
+#include <systems.hpp>
 
 /**
  * @namespace filter classes
@@ -49,6 +50,207 @@ namespace filter
 		float yk = t_tmp*(k*uk - yk_1) + yk_1;
 		return yk;
 	}
+
+	/**
+	 * Created by Nigel Redmon on 11/24/12
+	 * EarLevel Engineering: earlevel.com
+	 * Copyright 2012 Nigel Redmon
+	 *
+	 * For a complete explanation of the Biquad code:
+	 * http://www.earlevel.com/main/2012/11/26/biquad-c-source-code/
+	 *
+	 * License:
+	 *
+	 * This source code is provided as is, without warranty.
+	 * You may copy and distribute verbatim copies of this document.
+	 * You may modify and use this source code to create binary code
+	 * for your own purposes, free or commercial.
+	 *
+	 */
+
+	/**
+	 * Constructor of biquad second order IIR filter
+	 * @param type	type of filter
+	 * @param Fc	corner frequency normed to 1/(sampling frequency)
+	 * @param Q		quality factor of the filter response
+	 * @param peak_gain gain of the filter.
+	 */
+	biquad::biquad(const biquad_type_ec type, const float Fc, const float Q, const float peak_gain)
+	{
+		SetBiquad(type, Fc, Q, peak_gain);
+		z1 = z2 = 0.0f;
+	}
+
+    /**
+     * set type of biquad filter
+     * @param type new type of filter
+     */
+	void biquad::SetType(const biquad_type_ec type)
+	{
+		this->type = type;
+		CalcBiquad();
+	}
+
+    /**
+     * set the quality factor of the filter
+     * @param Q new quality factor
+     */
+    void biquad::SetQ(const float Q)
+	{
+		this->Q = Q;
+		CalcBiquad();
+	}
+
+    /**
+     * set new corner frequency
+     * @param Fc new corner frequency
+     */
+    void biquad::SetFc(const float Fc)
+    {
+		this->Fc = Fc;
+		CalcBiquad();
+	}
+
+    /**
+     * set new gain of the filter
+     * @param peak_gain in db
+     */
+    void biquad::SetPeakGain(const float peak_gain)
+    {
+		this->peak_gain = peak_gain;
+		CalcBiquad();
+	}
+
+    /**
+     * set all relevant biquad parameters at once
+     * @param type new type
+     * @param Fc new corner freq.
+     * @param Q new quality factor
+     * @param peak_gain new gain
+     */
+    void biquad::SetBiquad(const biquad_type_ec type, const float Fc, const float Q, const float peak_gain)
+    {
+		this->type = type;
+		this->Q = Q;
+		this->Fc = Fc;
+		SetPeakGain(peak_gain);
+	}
+
+    /**
+     * calculate internal coefficients
+     */
+    void biquad::CalcBiquad(void)
+    {
+		float norm;
+		float V = std::pow(10.0f, fabs(peak_gain) / 20.0f);
+		float K = std::tan(M_PI * Fc);
+
+		switch (this->type)
+		{
+		case biquad_type_ec::lowpass:
+
+			norm = 1.0f / (1.0f + K / Q + K * K);
+			a0 = K * K * norm;
+			a1 = 2.0f * a0;
+			a2 = a0;
+			b1 = 2.0f * (K * K - 1.0f) * norm;
+			b2 = (1.0f - K / Q + K * K) * norm;
+			break;
+
+		case biquad_type_ec::highpass:
+
+			norm = 1.0f / (1.0f + K / Q + K * K);
+			a0 = 1.0f * norm;
+			a1 = -2.0f * a0;
+			a2 = a0;
+			b1 = 2.0f * (K * K - 1.0f) * norm;
+			b2 = (1.0f - K / Q + K * K) * norm;
+			break;
+
+		case biquad_type_ec::bandpass:
+
+			norm = 1.0f / (1.0f + K / Q + K * K);
+			a0 = K / Q * norm;
+			a1 = 0.0f;
+			a2 = -a0;
+			b1 = 2.0f * (K * K - 1.0f) * norm;
+			b2 = (1.0f - K / Q + K * K) * norm;
+			break;
+
+		case biquad_type_ec::notch:
+
+			norm = 1.0f / (1.0f + K / Q + K * K);
+			a0 = (1.0f + K * K) * norm;
+			a1 = 2.0f * (K * K - 1) * norm;
+			a2 = a0;
+			b1 = a1;
+			b2 = (1.0f - K / Q + K * K) * norm;
+			break;
+
+		case biquad_type_ec::peak:
+
+			if (peak_gain >= 0.0f)
+			{    // boost
+				norm = 1.0f / (1.0f + 1.0f/Q * K + K * K);
+				a0 = (1.0f + V/Q * K + K * K) * norm;
+				a1 = 2.0f * (K * K - 1.0f) * norm;
+				a2 = (1.0f - V/Q * K + K * K) * norm;
+				b1 = a1;
+				b2 = (1.0f - 1.0f/Q * K + K * K) * norm;
+			}
+			else
+			{    // cut
+				norm = 1 / (1 + V/Q * K + K * K);
+				a0 = (1 + 1/Q * K + K * K) * norm;
+				a1 = 2 * (K * K - 1) * norm;
+				a2 = (1 - 1/Q * K + K * K) * norm;
+				b1 = a1;
+				b2 = (1 - V/Q * K + K * K) * norm;
+			}
+			break;
+		case biquad_type_ec::lowshelf:
+			if (peak_gain >= 0)
+			{    // boost
+				norm = 1.0f / (1 + math::SQRT2 * K + K * K);
+				a0 = (1.0f + sqrt(2.0f*V) * K + V * K * K) * norm;
+				a1 = 2.0f * (V * K * K - 1.0f) * norm;
+				a2 = (1.0f - sqrt(2.0f*V) * K + V * K * K) * norm;
+				b1 = 2.0f * (K * K - 1.0f) * norm;
+				b2 = (1.0f - math::SQRT2 * K + K * K) * norm;
+			}
+			else
+			{    // cut
+				norm = 1.0f / (1.0f + sqrt(2.0f*V) * K + V * K * K);
+				a0 = (1.0f + math::SQRT2 * K + K * K) * norm;
+				a1 = 2.0f * (K * K - 1.0f) * norm;
+				a2 = (1.0f - math::SQRT2 * K + K * K) * norm;
+				b1 = 2.0f * (V * K * K - 1.0f) * norm;
+				b2 = (1.0f - sqrt(2.0f*V) * K + V * K * K) * norm;
+			}
+			break;
+		case biquad_type_ec::highshelf:
+			if (peak_gain >= 0)
+			{    // boost
+				norm = 1.0f / (1.0f + math::SQRT2 * K + K * K);
+				a0 = (V + sqrt(2.0f*V) * K + K * K) * norm;
+				a1 = 2.0f * (K * K - V) * norm;
+				a2 = (V - sqrt(2.0f*V) * K + K * K) * norm;
+				b1 = 2.0f * (K * K - 1.0f) * norm;
+				b2 = (1.0f - math::SQRT2 * K + K * K) * norm;
+			}
+			else
+			{    // cut
+				norm = 1.0f / (V + sqrt(2.0f*V) * K + K * K);
+				a0 = (1.0f + math::SQRT2 * K + K * K) * norm;
+				a1 = 2.0f * (K * K - 1.0f) * norm;
+				a2 = (1.0f - math::SQRT2 * K + K * K) * norm;
+				b1 = 2.0f * (K * K - V) * norm;
+				b2 = (V - sqrt(2.0f*V) * K + K * K) * norm;
+			}
+			break;
+		}
+	}
+
 
 }/* namespace filter */
 
