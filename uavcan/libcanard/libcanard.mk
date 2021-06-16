3dc1cee1d402da2ab8309a1870d89d5bd3aca174
Alex B <tecnologic86@gmail.com>
2021-06-16 21:58:29 +0200

    added libcanard v1.1 and uavcan types

---
+++ uavcan/libcanard/libcanard.mk
@@ -0,0 +1,7 @@
+# List of all the related files.
+LIBCANARDSRC = ../../uavcan/libcanard/canard.c \
+                ../../uavcan/libcanard/canard_dsdl.c.c \
+
+# Required include directories
+LIBCANARDINC = ../../uavcan/libcanard
+		   
